ca2346cf05a4f1e647b82c35b1fc9ed3f270e974
cleantutorials <dev@cleantutorials.com>
2020-05-22 02:48:56 -0700

    Added Blocked Thread example

--- src/main/java/com/cleantutorials/jconsole/thread/ThreadState.java
+++ src/main/java/com/cleantutorials/jconsole/thread/ThreadState.java
@@ -4,37 +4,45 @@ import java.util.Set;
 
 /**
  * Creates multiple threads and starts them for demonstrating the state of the
- * thread. If you plan to run the example, please manually kill the process as there 
- * is no graceful exit in the program.
+ * thread. If you plan to run the example, please manually kill the process as
+ * there is no graceful exit in the program.
  */
 public class ThreadState {
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		RunnableThread runnable = new RunnableThread();
-		TimedWaitingThread timedWaiting = new TimedWaitingThread();
-		WaitingThread waiting = new WaitingThread();
-
 		runnable.setName("RUNNABLE Example");
 		runnable.start();
-		
+
+		WaitingThread waiting = new WaitingThread();
 		waiting.setName("WAITING Example");
 		waiting.start();
-		
+
+		TimedWaitingThread timedWaiting = new TimedWaitingThread();
 		timedWaiting.setName("TIMED_WAITING Example");
 		timedWaiting.start();
-				
+
+		Lock lock = new Lock();
+		lock.setName("Helper thread");
+		lock.start();
+
+		Blocked blocked = new Blocked();
+		blocked.setName("BLOCKED Example");
+		blocked.start();
+
 		printStackTrace();
-		
+
 	}
-	
+
 	/**
 	 * Prints the stack trace of all the threads.
 	 */
 	public static void printStackTrace() {
-		
+
 		// Get all threads in Java.
 		Set<Thread> threads = Thread.getAllStackTraces().keySet();
-		 
+
 		for (Thread thread : threads) {
 			// Print the thread name and current state of thread.
 			System.out.println("Thread Name:" + thread.getName());
@@ -89,6 +97,33 @@ class TimedWaitingThread extends Thread {
 			Thread.sleep(100000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		}
+	}
+}
+
+/**
+ * Blocked represents a thread in BLOCKED state.
+ */
+class Blocked extends Thread {
+	@Override
+	public void run() {
+		synchronized (Lock.class) {
+		}
+	}
+}
+
+/**
+ * Lock is a helper thread that helps demonstrate Blocked state.
+ */
+class Lock extends Thread {
+	@Override
+	public void run() {
+		synchronized (Lock.class) {
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
