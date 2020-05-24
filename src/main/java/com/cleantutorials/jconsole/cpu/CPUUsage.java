cba8ca5612f3a8d108ecd41643c7a8a8215cfee7
cleantutorials <dev@cleantutorials.com>
2020-05-24 01:10:42 -0700

    Generate only limited random numbers

--- src/main/java/com/cleantutorials/jconsole/cpu/CPUUsage.java
+++ src/main/java/com/cleantutorials/jconsole/cpu/CPUUsage.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 import java.util.Random;
 
 /**
- * A class to demonstrate the CPU usage in the system space and the user space as seen in JConsole.
- * Note: Only to demonstrate an example, manually kill the process if you want to run the example or it can eat up
- * a lot of resources.
+ * A class to demonstrate the CPU usage in the system space and the user space
+ * as seen in JConsole. Note: Only to demonstrate an example, manually kill the
+ * process if you want to run the example or it can eat up a lot of resources.
  */
 public class CPUUsage {
 	public static void main(String[] args) throws IOException, InterruptedException {
@@ -20,23 +20,26 @@ public class CPUUsage {
 }
 
 /**
- * Thread that generates random number and write it to a file replacing the older content. Used in demonstrating the 
- * CPU time and User time in JConsole.
+ * Thread that generates lots of random number and write it to a file.
+ * Used in demonstrating the CPU time and User time in JConsole.
  */
 class GenerateRandomNumner extends Thread {
 	@Override
-	public void run() {	
+	public void run() {
 		File file = new File("random_numbers.txt");
- 		Random rand = new Random();
- 		try (BufferedWriter br = new BufferedWriter(new FileWriter(file))) {
- 			while(true) {
- 				// Contributes to the time executing calls in user space.
- 				String randomNumer = Integer.toString(rand.nextInt());
-	 			// Contributes to the time executing calls in kernel space
- 				br.write(randomNumer);	 				
- 			}
- 		} catch (Exception e) {
+		Random rand = new Random();
+
+		try (BufferedWriter br = new BufferedWriter(new FileWriter(file))) {
+			for (int i = 0; i < 10000000; i++) {
+				// Contributes to the time executing calls in user space.
+				String randomNumer = Integer.toString(rand.nextInt());
+				// Contributes to the time executing calls in kernel space.
+				br.write(randomNumer + ",");
+				// Flush the buffer.
+				br.flush();
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}	
+	}
 }
